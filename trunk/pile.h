b77568a3eb0e6f122839477a36d9957e44abd414
i.lagouilly@gmail.com <i.lagouilly@gmail.com@96ccf566-6db9-658b-9772-6114bd66d00a>
2012-05-18 10:17:39 +0000

    J'ajoute le fichier pile.h qui contient la declaration plus les fonctions pour necessaires pour manipuler notre pile.

---
+++ trunk/pile.h
@@ -0,0 +1,37 @@
+#include "manager.h"
+#define TAILLE_MAX valeur  // /!\ à définir
+
+struct Pile 
+{
+	Coord tab[TAILLE_MAX]; // tableau qui contient les coordonnees
+	int id_queue; // indice du dernier element du tableau
+};
+
+Coord initialiserCoord() 
+{
+	Coord c;
+	c.x = -1;
+	c.y = -1;
+return c;
+}
+Pile creerPile()
+{
+	Pile p;
+	p.tab[0] = initialiserCoord();
+	p.id_queue = 0;
+return p;
+}
+
+Pile empiler(Pile p, Coord c)
+{
+	p.id_queue++;
+	p.tab[p.id_queue] = c;
+return p;
+}
+
+Pile depiler(Pile p)
+{
+	p.tab[id_queue] = initialiserCoord();
+	p.id_queue--;
+return p;
+}
