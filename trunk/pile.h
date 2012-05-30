2e5366861cd49a75ef99b6135e760048b96d2f94
guparan@gmail.com <guparan@gmail.com@96ccf566-6db9-658b-9772-6114bd66d00a>
2012-05-30 15:14:25 +0000

    Ajout du rapport et paufinement des taches principales

--- trunk/pile.h
+++ trunk/pile.h
@@ -9,8 +9,8 @@ void pile_initPile()
 	int i,j;
 	for(i=0 ; i<TAILLE_PILE ; ++i)
 	{		
-		pile_pivots[i].coord.x = -1;
-		pile_pivots[i].coord.y = -1;
+		//pile_pivots[i].coord.x = -1;
+		//pile_pivots[i].coord.y = -1;
 		for(j=0 ; j<4 ; j++)
 		{
 			pile_pivots[i].possibilites[j] = true;
@@ -22,8 +22,8 @@ void pile_initPile()
 void pile_empiler(int x, int y, bool gauche, bool droite, bool haut, bool bas)
 {
 	pile_pivots_tete++;
-	pile_pivots[pile_pivots_tete].coord.x = x;
-	pile_pivots[pile_pivots_tete].coord.y = y;
+	//pile_pivots[pile_pivots_tete].coord.x = x;
+	//pile_pivots[pile_pivots_tete].coord.y = y;
 	pile_pivots[pile_pivots_tete].possibilites[dir_gauche]=gauche;
 	pile_pivots[pile_pivots_tete].possibilites[dir_droite]=droite;
 	pile_pivots[pile_pivots_tete].possibilites[dir_haut]=haut;
@@ -35,8 +35,8 @@ int pile_depiler()
 	int j;
 	if(pile_pivots_tete >= 0)
 	{
-		pile_pivots[pile_pivots_tete].coord.x = -1;
-		pile_pivots[pile_pivots_tete].coord.y = -1;
+		//pile_pivots[pile_pivots_tete].coord.x = -1;
+		//pile_pivots[pile_pivots_tete].coord.y = -1;
 		for(j=0 ; j<4 ; j++)
 		{
 			pile_pivots[pile_pivots_tete].possibilites[j] = true;
@@ -59,9 +59,9 @@ void pile_editer(Direction d, bool val)
 	pile_depiler();
 }
 
-Case pile_top()
+Pivot pile_top()
 {	
-	return pile_pivots[pile_pivots_tete].coord;
+	return pile_pivots[pile_pivots_tete];
 }
 
 int pile_getPremierePossibilite()
